c990228d8f5c880fb45c932d2cc6b4be97b347a2
Denis Petrovskii <denpetrovsky369@gmail.com>
2023-01-22 11:40:24 +0300

    work_on_mistakes

--- src/test/java/ru/netology/CardDelyveryOrderTest.java
+++ src/test/java/ru/netology/CardDelyveryOrderTest.java
@@ -1,23 +1,38 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDelyveryOrderTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void test() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $x("//input[@placeholder=\"Город\"]").setValue("Москва");
-        $("[type=\"tel\"]").setValue("21.01.2023");
+        String planningDate = generateDate(3);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[name=\"name\"]").setValue("Иванов-Соколов Иван");
         $("[name=\"phone\"]").setValue("+79099099999");
         $x("//span[@class=\"checkbox__box\"]").click();
         $x("//span[@class=\"button__text\"]").click();
         $x("//*[contains(text(),\"Успешно!\")]").hover();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 }
